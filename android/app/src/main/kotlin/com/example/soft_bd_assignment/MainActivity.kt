0d33a9ba2472426bd3695305bbd57ea1741b40ac
Shibam Dey <shibam.dey.sd@gmail.com>
2024-07-09 18:02:26 +0600

    First commit

---
+++ android/app/src/main/kotlin/com/example/soft_bd_assignment/MainActivity.kt
@@ -0,0 +1,5 @@
+package com.example.soft_bd_assignment
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity()
---
+++ lib/Screen/home_screen.dart
@@ -0,0 +1,30 @@
+import 'package:flutter/material.dart';
+
+class HomeScreen extends StatefulWidget {
+  const HomeScreen({super.key});
+
+  @override
+  State<HomeScreen> createState() => _HomeScreenState();
+}
+
+class _HomeScreenState extends State<HomeScreen> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        leading: IconButton(
+          onPressed: () {},
+          icon: Icon(Icons.menu),
+        ),
+        title: Row(
+          children: [
+            Text(
+              "Flutter Task",
+              style: TextStyle(fontWeight: FontWeight.bold),
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
